2eb17d4c2ad0fcc66b90444670f1b4ec90d091aa
Jiwon Kang <jiwonk42@gmail.com>
2017-04-21 09:26:40 -0700

    create a Product Table

---
+++ src/gummi-bear-kingdom/Migrations/20170421162053_MakeTableNamesPlural.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gummibearkingdom.Migrations
+{
+    public partial class MakeTableNamesPlural : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- src/gummi-bear-kingdom/Models/Product.cs
+++ src/gummi-bear-kingdom/Models/Product.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace gummi_bear_kingdom.Models
 {
+    [Table("Products")]
     public class Product
     {
         [Key]
