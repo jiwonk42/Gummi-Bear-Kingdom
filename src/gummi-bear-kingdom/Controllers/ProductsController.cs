e55bd5c3d6d56108dca2e4b4ece31f1e52c6bf4f
Jiwon Kang <jiwonk42@gmail.com>
2017-04-21 11:04:14 -0700

    add Edit functionality

--- src/gummi-bear-kingdom/Controllers/ProductsController.cs
+++ src/gummi-bear-kingdom/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore
 using gummi_bear_kingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -33,6 +34,20 @@ namespace gummi_bear_kingdom.Controllers
         public IActionResult Create(Product product)
         {
             db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
