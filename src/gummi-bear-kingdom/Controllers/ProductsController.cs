38a3711aeb5c8f9cae3330a3c17a381142c5407e
Jiwon Kang <jiwonk42@gmail.com>
2017-04-21 10:04:31 -0700

    enter data

--- src/gummi-bear-kingdom/Controllers/ProductsController.cs
+++ src/gummi-bear-kingdom/Controllers/ProductsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using gummi_bear_kingdom.Models;
-using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
